97a404fd1a6376cc52775456848f83c643a524a5
John Brady <johntbrady01@gmail.com>
2023-03-31 12:49:10 -0400

    first

---
+++ src/components/tickets/TicketForm.js
@@ -0,0 +1,101 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
+export const TicketForm = () => {
+    /*
+        TODO: Add the correct default properties to the
+        initial state object
+    */
+    const [ticket, update] = useState({
+        description:"",
+        emergency:false
+    })
+    /*
+        TODO: Use the useNavigation() hook so you can redirect
+        the user to the ticket list
+    */
+
+        const navigate=useNavigate()
+
+    const localHoneyUser = localStorage.getItem("honey_user")
+    const honeyUserObject = JSON.parse(localHoneyUser)
+
+    const handleSaveButtonClick = (event) => {
+        event.preventDefault()
+
+        // TODO: Create the object to be saved to the API
+        /*
+                   {
+                    "userId": 2,
+                    "description": "A deleniti est sed vel. Dolores aliquid enim vero. Quia eligendi vel voluptas. Nihil nihil quasi ullam officia doloremque amet non. Officia atque quae.",
+                    "emergency": false,
+                    "dateCompleted": ""
+                   }
+
+        */
+                   const ticketToSendToApi ={
+                    userId:honeyUserObject.id,
+                    description:ticket.description,
+                    emergency:ticket.emergency,
+                    dateCompleted:""
+                   }
+
+        // TODO: Perform the fetch() to POST the object to the API
+                   return fetch(`http://localhost:8088/serviceTickets`, {
+                            method:"POST",
+                            headers: {
+                                "Content-Type":"application/json"
+                            },
+                            body: JSON.stringify(ticketToSendToApi)
+
+                   })
+                   .then(response => response.json())
+                   .then(() => {
+                        navigate("/tickets")
+                   }) 
+                }
+
+    return (
+        <form className="ticketForm">
+            <h2 className="ticketForm__title">New Service Ticket</h2>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="description">Description:</label>
+                    <input
+                        required autoFocus
+                        type="text"
+                        className="form-control"
+                        placeholder="Brief description of problem"
+                        value={ticket.description}
+                        onChange={
+                            (evt)=>{
+                                const copy = {...ticket}
+                                copy.description=evt.target.value
+                                update(copy)
+                            }
+                        } />
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="name">Emergency:</label>
+                    <input type="checkbox"
+                        value={ticket.emergency}
+                        onChange={
+                            (evt)=>{
+                                const copy = {...ticket}
+                                copy.emergency=evt.target.checked
+                                update(copy)
+                            }
+                        } />
+                </div>
+            </fieldset>
+            <button 
+                onClick={(clickEvent)=>{handleSaveButtonClick(clickEvent)}  }
+            
+            className="btn btn-primary">
+                Submit Ticket
+            </button>
+        </form>
+    )
+}
